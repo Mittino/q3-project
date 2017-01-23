c0b9e3bcc93683e460cb441dcb78ddf74791e08d
mittino <annamefford@gmail.com>
2017-01-23 13:18:50 -0700

    added component files and building out app.js

---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import request from 'superagent';
+import { connect } from 'react-redux';
+import Comment from './Comment.js';
+import Landing from './Landing.js';
+import Nav from './Nav.js';
+import NewPostForm from './NewPostForm.js';
+import Post from './Post.js';
+import PostsContainer from './PostsContainer';
+import Profile from './Profile';
+
+const App = React.createClass({
+
+  render(){
+    <div>Test</div>
+}
+
+});
+
+export default App;
