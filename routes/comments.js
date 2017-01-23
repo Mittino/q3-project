69a3884eb21c6538e17ca3ae888ff1332bf30806
Cassie Peterson <cas.j.peters@gmail.com>
2017-01-23 15:46:57 -0700

    comments get route works

--- routes/comments.js
+++ routes/comments.js
@@ -11,22 +11,34 @@ const {camelizeKeys, decamelizeKeys} = require('humps');
 //ROUTES ------------------------------------------------
 
 //get all comments
-router.get('/', (req, res) => {
-  knex('comments')
-  .orderBy('id')
-  .then((data) => {
-    res.send(data);
-  });
-});
+// router.get('/', (req, res) => {
+//   knex('comments')
+//   .orderBy('id')
+//   .then((data) => {
+//     res.send(data);
+//   });
+// });
 
 //get comments by user_id (possibly so users can see their own comments)
-router.get('/user/:id', (req, res) => {
-  knex('comments')
-  .where('user_id', req.params.id)
+// router.get('/user/:id', (req, res) => {
+//   knex('comments')
+//   .where('user_id', req.params.id)
+//   .then((data) => {
+//     res.send(data);
+//   });
+// });
+
+//get comments by post id
+router.get('/:postId', (req, res) => {
+  knex('posts')
+  .where('posts.id', req.params.postId)
+  .join('comments', 'posts.id', '=', 'comments.post_id')
+  .join('users', 'comments.user_id', '=', 'users.id')
+  .select('posts.id', 'comment_body', 'comments.created_at', 'users.profile_url', 'users.username', 'comments.user_id')
   .then((data) => {
-    res.send(data);
-  });
-});
+    res.send(data)
+  })
+})
 
 
 // EXPORTS ---------------------------
--- seeds/4-comments.js
+++ seeds/4-comments.js
@@ -11,6 +11,24 @@ exports.seed = function(knex, Promise) {
           user_id: 1,
           post_id: 2,
           comment_body: 'Yo snoop its ya homie Jeremy. Ive been working on my skillzz and I would be up to the task.'
+        }),
+        knex('comments').insert({
+          id: 2,
+          user_id: 1,
+          post_id: 2,
+          comment_body: 'Yo snoop its ya homie Jeremy, again. Ive got some sick new beats to drop on yo face.'
+        }),
+        knex('comments').insert({
+          id: 3,
+          user_id: 1,
+          post_id: 2,
+          comment_body: 'Yo snoop its ya homie Jeremy ... yet again. You aint done returned my calls B, where is da luv?'
+        }),
+        knex('comments').insert({
+          id: 4,
+          user_id: 2,
+          post_id: 1,
+          comment_body: 'Yo its snoop-lion here. Everyone check out my new dope ass EP!'
         })
         .then(() => {
           return knex.raw("SELECT setval('comments_id_seq', (SELECT MAX(id) FROM comments))");
