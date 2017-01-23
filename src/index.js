6b5f0a12586353f777029bc89fd5f471f40f2955
mittino <annamefford@gmail.com>
2017-01-23 13:26:20 -0700

    updating reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store form './store';
+import store from './store';
 import App from './components/App';
-import './index.css';
+//import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
