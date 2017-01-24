aa5233cc91175345c08358fc5b7a8f5073d2e673
Evan Busse <evanbusse@gmail.com>
2017-01-24 14:52:46 -0700

    null

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import { Provider } from 'react-redux';
 //import store from './store';
-import App from './components/App.js';
-//import './index.css';
+import App from './components/app.js';
+import './index.css';
 
 ReactDOM.render(
   //<Provider store={store}>
