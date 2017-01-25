6d54829150fa5e1cb677ff0b698e84a0d0ba1042
mittino <annamefford@gmail.com>
2017-01-25 15:05:42 -0700

    fixed cors issue

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 
 var port = 5000;
+var cors = require('cors');
+app.use(cors);
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
